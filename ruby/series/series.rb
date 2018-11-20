8315572329dd8310672bb483aea6c181d7abf025
veronicacannon <me@veronicacannon.com>
2018-11-20 21:23:06 +0000

    Add series exercise

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,20 +1,11 @@
-# Return series of number from string of digits
 class Series
   def initialize(digits)
     @digits = digits
   end
 
-  # build a substring from digits and provided length
   def slices(substring_length)
-    digits_length = @digits.length
-    raise ArgumentError if substring_length > digits_length
+    raise ArgumentError if substring_length > @digits.length
 
-    results = []
-    digits_length.times do |i|
-      break if i + substring_length > digits_length
-
-      results.push(@digits[i, substring_length])
-    end
-    return results
+   @digits.each_char.each_cons(substring_length).map(&:join)
   end
 end
