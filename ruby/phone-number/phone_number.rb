9327b65164448696c1a7da349ee0c504f2961df1
veronicacannon <me@veronicacannon.com>
2018-11-21 16:12:35 +0000

    Submit phone-number

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,8 +1,6 @@
-# Clean Up Phone Numbers
 module PhoneNumber
   VALID_PHONE_NUMBER = /^([2-9]\d\d){2}\d{4}$/
 
-  # Use North American Numbering Plan (NANP) to Validate
   def self.clean(number)
     numbers = number.gsub(/\D/, '').sub(/^1/, '')
     numbers[VALID_PHONE_NUMBER]
